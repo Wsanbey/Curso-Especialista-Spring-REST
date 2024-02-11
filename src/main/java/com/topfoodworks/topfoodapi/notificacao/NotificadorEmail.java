d28dacd8ee8763291e67b3afa695406bb95103ac
Wsanbey <welrysanbey@hotmail.com>
2024-02-11 15:33:25 -0300

    2.17. Desambiguação de beans com @Primary

--- src/main/java/com/topfoodworks/topfoodapi/notificacao/NotificadorEmail.java
+++ src/main/java/com/topfoodworks/topfoodapi/notificacao/NotificadorEmail.java
@@ -1,8 +1,10 @@
 package com.topfoodworks.topfoodapi.notificacao;
 
 import com.topfoodworks.topfoodapi.modelo.Cliente;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+@Primary
 @Component
 public class NotificadorEmail implements Notificador {
 
